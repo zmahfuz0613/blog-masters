017f369aa3e40a33f40219144f3ad4a91e14f85e
Barnett Lobel <lobelbarnett@gmail.com>
2020-08-17 14:33:58 -0400

    setting up routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,13 +3,20 @@ import { Route, Switch } from 'react-router-dom'
 import './App.css'
 import Nav from './components/shared/Nav/Nav'
 import Footer from './components/shared/Footer/Footer'
+import Home from './screens/Home/Home'
+import PostDetail from './screens/PostDetail/PostDetail'
+import PostCreate from './screens/PostCreate/PostCreate'
+import PostEdit from './screens/PostEdit/PostEdit'
 
 function App() {
   return (
     <div className="app">
       <Nav />
       <Switch>
-        <main>BLOG HOME</main>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/:id" component={PostDetail} />
+        <Route exact path="/create-post" component={PostCreate} />
+        <Route exact path="/edit-post" component={PostEdit} />
       </Switch>
       <Footer />
     </div>
