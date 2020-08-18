db68d8aa801db275c716ce2f7a0480c34eb0651a
Barnett Lobel <lobelbarnett@gmail.com>
2020-08-17 21:12:38 -0400

    add newspaper font

--- client/src/screens/PostDetail/PostDetail.jsx
+++ client/src/screens/PostDetail/PostDetail.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
 import './PostDetail.css'
+import { getPost, deletePost } from '../../services/posts'
 
-function PostDetail() {
+function PostDetail(props) {
+  console.log(props)
+  const [post, setPost] = useState(null)
+  const [isLoaded, setLoaded] = useState(false)
+  const { id } = useParams()
+
+  useEffect(() => {
+    const fetchPost = async () => {
+      const post = await getPost(id)
+      setPost(post)
+      setLoaded(true)
+    }
+    fetchPost()
+  }, [id])
+
+  if (!isLoaded) {
+    return <h1>Loading... Please wait.</h1>
+  }
+
   return (
-    <div>
-      <h1>TESTING</h1>
-      <h1>POST DETAIL</h1>
+    <div className="post-detail">
+      <h1 className="post-detail__title">{post.title}</h1>
+      <h2 className="post-detail__author">{post.author}</h2>
+      <img className="post-detail__image" src={post.imgURL} alt={post.title} />
+      <p>{post.body}</p>
     </div>
   )
 }
