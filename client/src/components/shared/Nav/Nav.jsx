4216b95cab9b536db8a4edfb9d3b06951a0990ae
Barnett Lobel <lobelbarnett@gmail.com>
2020-08-18 08:19:13 -0400

    fix posts link

--- client/src/components/shared/Nav/Nav.jsx
+++ client/src/components/shared/Nav/Nav.jsx
@@ -1,30 +1,35 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import './Nav.css'
-import { Navbar } from 'react-bootstrap';
+import { Navbar } from 'react-bootstrap'
 
 function Nav() {
   return (
     <>
-    <Navbar collapseOnSelect expand="lg" color="#ff4000">
+      <Navbar collapseOnSelect expand="lg" color="#ff4000">
         <Navbar.Brand>
           <Link to="/">BLOG MASTER 3000</Link>
         </Navbar.Brand>
-    <Navbar.Toggle aria-controls="responsive-Navbar-nav" />
-    <Navbar.Collapse id="responsive-Navbar-nav">
-    <Navbar className="mr-auto">      
-     </Navbar>
-     <Navbar>      
-            <Link className="nav-link nav-link-important" to="/posts">Posts</Link>
-            <Link className="nav-link nav-link-important" to="/create-post">Create Post</Link>
-            <Link className="nav-link nav-link-important" to="/footer">Contact us</Link>
-            <Link className="nav-link nav-link-important" to="#">sign up</Link>
-
-     </Navbar>
-    
-    </Navbar.Collapse>
-    </Navbar>      
-         </>
+        <Navbar.Toggle aria-controls="responsive-Navbar-nav" />
+        <Navbar.Collapse id="responsive-Navbar-nav">
+          <Navbar className="mr-auto"></Navbar>
+          <Navbar>
+            <Link className="nav-link nav-link-important" to="/">
+              Posts
+            </Link>
+            <Link className="nav-link nav-link-important" to="/create-post">
+              Create Post
+            </Link>
+            <Link className="nav-link nav-link-important" to="/footer">
+              Contact us
+            </Link>
+            <Link className="nav-link nav-link-important" to="#">
+              sign up
+            </Link>
+          </Navbar>
+        </Navbar.Collapse>
+      </Navbar>
+    </>
   )
 }
 
